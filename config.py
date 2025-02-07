d997f432473f58186e7b433964a425702d25f3b5
DaniilSeliverstov <165933771+DannilSeliverstov@users.noreply.github.com>
2025-02-07 13:38:57 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '...'
-
-IMAGE_FOLDER = "images/"
-
-SUPPORT_EMAIL = "info@zoo.ru"
-SUPPORT_PHONE = "+7 123 456 7890"
-
-GUARDIANSHIP_LINK = "https://moscowzoo.ru/about/guardianship"
-BOT_SHARE_LINK = "https://t.me/..."
